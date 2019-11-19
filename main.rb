9479589466a3638dbe6faa11d546e9a56f69b3bd
Your Actual Name <Your Actual Email>
2019-11-19 14:19:53 +0900

    NEW - POSTのコールバックを記述

--- main.rb
+++ main.rb
@@ -13,3 +13,20 @@ get '/callback' do
 
   params["hub.challenge"]
 end
+
+post '/callback' do
+  hash = JSON.parse(request.body.read)
+  message = hash["entry"][0]["messaging"][0]
+  sender = message["sender"]["id"]
+  text = message["message"]["text"]
+  endpoint = ENV["TOKEN_URL"]
+  content = {
+    recipient: { id: sender },
+    message: { text: text }
+  }
+  request_body = content.to_json
+
+  RestClient.post endpoint, request_body, content_type: :json, accept: :json
+  status 201
+  body ''
+end
