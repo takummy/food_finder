d546b0d3ae9fb118c6161ef911b33c45e81cd917
takummy <40820363+takummy@users.noreply.github.com>
2019-11-19 09:32:01 +0900

    Feature/create simple bot#2 (#7)
    * NEW - コールバックURLを作成
    
    * NEW - Rackの設定ファイルを作成
    
    * ADD - AWS Lambdaを想定し、rubyのバージョンをダウングレード

--- main.rb
+++ main.rb
@@ -4,3 +4,11 @@ require 'sinatra-reloader'
 get '/' do
   "Hello, world!"
 end
+
+get '/callback' do
+  if params["hub.verify_token"] != 'hogehoge'
+    return 'Error, wrong validation token.'
+  end
+
+  params["hub.challenge"]
+end
