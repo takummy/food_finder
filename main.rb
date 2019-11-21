bbe99bb3ed4421716ee355d78232a1275e718b66
Your Actual Name <Your Actual Email>
2019-11-21 16:17:08 +0900

    ADD - ぐるなびAPIのエンドポイントを定数化

--- main.rb
+++ main.rb
@@ -6,6 +6,8 @@ require 'sinatra/reloader'
 Dotenv.load
 
 FB_ENDPOINT = ENV["TOKEN_URL"]
+GNAVI_KEYID = ENV["GNAVI_KEYID"]
+GNAVI_CATEGORY_LARGE_SEARCH_API = "https://api.gnavi.co.jp/master/CategoryLargeSearchAPI/v3/"
 
 get '/' do
   "Hello, world!"
@@ -23,6 +25,8 @@ post '/callback' do
   hash = JSON.parse(request.body.read)
   message = hash["entry"][0]["messaging"][0]
   sender = message["sender"]["id"]
+
+  if message["message"]["text"] == "レストラン検索"
   text = "カテゴリーと位置情報からレストランを検索します。レストランを検索したい場合は、「レストラン検索」と話しかけてね！"
   content = {
     recipient: { id: sender },
