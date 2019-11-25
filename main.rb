879267025f51a59ffb0f12735ee4276d08aa26f3
takummy <40820363+takummy@users.noreply.github.com>
2019-11-25 10:56:12 +0900

    Feature/create category serch 2#3 (#18)
    * ADD - ぐるなびAPIのエンドポイントを定数化
    
    * ADD - カテゴリー一覧取得用のメソッドを定義
    
    * ADD - カテゴリー一覧をフィルタリングするメソッドを定義
    
    * ADD - MEssenger送信の前処理用のメソッドを定義
    
    * ADD - POSTコールバックのアクションを修正
    
    * ADD - 進捗を更新

--- main.rb
+++ main.rb
@@ -6,6 +6,46 @@ require 'sinatra/reloader'
 Dotenv.load
 
 FB_ENDPOINT = ENV["TOKEN_URL"]
+GNAVI_KEYID = ENV["GNAVI_KEYID"]
+GNAVI_CATEGORY_LARGE_SEARCH_API = "https://api.gnavi.co.jp/master/CategoryLargeSearchAPI/v3/"
+
+helpers do
+  def get_categories
+    response = JSON.parse(RestClient.get GNAVI_CATEGORY_LARGE_SEARCH_API + "?keyid=" + GNAVI_KEYID)
+    categories = response["category_l"]
+    categories
+  end
+
+  def filter_categories
+    categories = []
+    get_categories.each_with_index do |category, i|
+      if i < 11
+        hash = {
+          content_type: 'text',
+          title: category["category_l_name"],
+          payload: category["category_l_code"],
+        }
+        p hash
+        categories.push(hash)
+      else
+        p "11回目は配列に入れない"
+      end
+    end
+    categories
+  end
+
+  def set_quick_reply_of_categories(sender, categories)
+    {
+      recipient: {
+        id: sender
+      },
+      message: {
+        text: "何が食べたいですか？",
+        quick_replies: categories
+      }
+    }.to_json
+  end
+end
 
 get '/' do
   "Hello, world!"
@@ -23,14 +63,23 @@ post '/callback' do
   hash = JSON.parse(request.body.read)
   message = hash["entry"][0]["messaging"][0]
   sender = message["sender"]["id"]
-  text = "カテゴリーと位置情報からレストランを検索します。レストランを検索したい場合は、「レストラン検索」と話しかけてね！"
-  content = {
-    recipient: { id: sender },
-    message: { text: text }
-  }
-  request_body = content.to_json
 
-  RestClient.post FB_ENDPOINT, request_body, content_type: :json, accept: :json
+  if message["message"]["text"] == "レストラン検索"
+
+    categories = filter_categories
+    request_body = set_quick_reply_of_categories(sender, categories)
+    RestClient.post FB_ENDPOINT, request_body, content_type: :json, accept: :json
+
+  else
+    text = "カテゴリーと位置情報からレストランを検索します。レストランを検索したい場合は、「レストラン検索」と話しかけてね！"
+    content = {
+      recipient: { id: sender },
+      message: { text: text }
+    }
+    request_body = content.to_json
+
+    RestClient.post FB_ENDPOINT, request_body, content_type: :json, accept: :json
+  end
   status 201
   body ''
 end
